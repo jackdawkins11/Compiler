3c5fa12e85ba6230bb5067289179227ebb44f54e
John Dawkins <jack>
2019-05-18 09:56:22 -0500

    Added comments to SymbolTable.java

--- src/symbolTable/SymbolTable.java
+++ src/symbolTable/SymbolTable.java
@@ -4,11 +4,33 @@ package symbolTable;
 import java.util.HashMap;
 import syntaxTree.*;
 
+/*
+ * This class provides a HashMap
+ * from ID strings to the nodes
+ * created from previously parsed
+ * ID names. It is used to make sure
+ * variables/functions are declared
+ * before they are used.
+ */
+
 public class SymbolTable{
 
+	/*
+	 * A ID to VariableNode HashMap.
+	 */
+
 	HashMap< String, VariableNode > variables = new HashMap< String, VariableNode >();
-	
+
+	/*
+	 * A ID to SubProgramNode HashMap.
+	 */
+
 	HashMap< String, SubProgramNode > subPrograms = new HashMap< String, SubProgramNode >();
+
+	/*
+	 * Print all of the IDs in the
+	 * HashMaps.
+	 */
 
 	public void print(){
 
@@ -26,6 +48,10 @@ public class SymbolTable{
 	
 	}
 
+	/*
+	 * Add elements to the HashMap.
+	 */
+
 	public void addVariable( VariableNode variableNode ){
 
 		variables.put( variableNode.getName(), variableNode );
@@ -37,6 +63,11 @@ public class SymbolTable{
 		subPrograms.put( subProgramNode.getName(), subProgramNode );
 
 	}
+
+	/*
+	 * Check if IDs have been parsed
+	 * already.
+	 */
 
 	public boolean isVariableName( String name ){
 
@@ -65,6 +96,10 @@ public class SymbolTable{
 		}
 
 	}
+
+	/*
+	 * Get the node associated with an ID.
+	 */
 
 	public VariableNode getVariableByName( String name ){
 
